9f670411f49d0893aef5f7659924081d04ef66ec
taufiqx404 <59577411+taufiqx404@users.noreply.github.com>
2020-10-20 19:55:28 +0800

    Create code.sh

---
+++ code.sh
@@ -0,0 +1,258 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+#
+figlet -f slant " WELCOME "|lolcat
+sleep 2
+echo $ij"____________________________________________________________________________"
+echo $pr"╔╗─╔╗╔═══╗╔═══╗╔╗╔═╗╔══╗╔═╗─╔╗╔═══╗"                                                        
+echo $pr"║║─║║║╔═╗║║╔═╗║║║║╔╝╚╣─╝║║╚╗║║║╔═╗║"
+echo $cy"║╚═╝║║║─║║║║─╚╝║╚╝╝──║║─║╔╗╚╝║║║─╚╝"
+echo $cy"║╔═╗║║╚═╝║║║─╔╗║╔╗║──║║─║║╚╗║║║║╔═╗"
+echo $ku"║║─║║║╔═╗║║╚═╝║║║║╚╗╔╣─╗║║─║║║║╚╩═║"
+echo $ku"╚╝─╚╝╚╝─╚╝╚═══╝╚╝╚═╝╚══╝╚╝─╚═╝╚═══╝"
+echo $ij"_____________________________________________________________________________"
+sleep 2
+clear
+echo $me"───▄▄▄▄▄▄─────▄▄▄▄▄▄"
+sleep 1
+echo $me"─▄█▓▓▓▓▓▓█▄─▄█▓▓▓▓▓▓█▄"
+sleep 1
+echo $me"▐█▓▓     ▓▓█▓▓     ▓▓█▌"
+echo $me"█▓▓                 ▓▓█"
+echo $ku"█▓▓FEBRI  💓        ▓▓█"
+sleep 1
+echo $me"▐█▓▓                ▓▓█▌"
+echo $ku"─▀█▓▓       TAUFIQ ▓▓█▀"
+echo $me"───▀█▓▓         ▓▓█▀"
+sleep 1
+echo $me"─────▀█▓▓     ▓▓█▀"
+sleep 1
+echo $me"──────▀█▓    █▀"
+sleep 1
+echo $me"────────▀█▓█▀"
+sleep 1
+echo $me"──────────▀"
+sleep 2
+clear
+read -p "SIAPA NAMA ANDA =>" pil
+sleep 2
+echo $pu"SELAMAT DATANG DI TOOLS CODE"                                                             
+ sleep 1
+echo $me"*                   *"
+sleep 1
+echo $me"    *  .:okOOOkdc;.       * .;cdkOOOko:. *"
+sleep 1
+echo $me"     .xOOOOOOOOOOOOc       cOOOOOOOOOOOOx."
+sleep 1
+echo $me" *  :OOOOOOOOOOOOOOOk,*  ,kOOOOOOOOOOOOOOO:"
+sleep 1
+echo $me"   :OOOOOOOOOOOOOOOOOO: :OOOOOOOOOOOOOOOOOO: *"
+sleep 1
+echo $me"   oOOOOOOOOOOOOOOOOOOOoOOOOOOOOOOOOOOOOOOOo"
+sleep 1
+echo $cy"●■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■●"
+sleep 1
+echo $cy"■######            ONLY YOU B3B         ######■"
+sleep 1
+echo $me"■#############################################■"
+sleep 1
+echo $pr"■##########           BUCIN         ##########■"
+sleep 1
+echo $me"■#############################################■"
+sleep 1
+echo $pr"■###                  FEBRI               ####■"
+sleep 1
+echo $me"■#############################################■"
+sleep 1
+echo $cy"●■■■■■■■■■■■■■■●  SAYANG KAMU ●■■■■■■■■■■■■■■●"
+sleep 1
+echo $me"          :OOOOOOOOOOOOOOOOOOOOOOOOO: *"
+sleep 1
+echo $me"   *        :oOOOOOOOOOOOOOOOOOOOo:         *"
+sleep 1
+echo $me" *            :kOOOOOOOOOOOOOOOk:       *"
+sleep 1
+echo $me"            *   :xOOOOOOOOOOOx:          *"                                                 
+sleep 1
+echo $me"    *             :lOOOOOOOl:    *"
+sleep 1
+echo $me"     *               :dOd:             *"
+sleep 1
+echo $me"               *       ."
+echo $pu"___________________________________________________"
+echo $cy"••••••••••••••"
+echo $ij"1.DDOS ATTACK"
+echo $cy"••••••••••••••"
+echo $pr"♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡"
+sleep 1
+echo $ij"••••••••••••••"
+echo $pr"2.VIRUS "
+echo $ij"••••••••••••••"
+echo $pu"♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡"
+sleep 1
+echo $me"•••••••••••••"
+echo $ku"3.HACK CCTV "
+echo $me"•••••••••••••"
+echo $ij"♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡"
+sleep 1
+echo $pu"•••••••••••••"
+echo $me"4.IP SCAN"
+echo $pu"•••••••••••••"
+echo $ku"♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡"
+sleep 1
+echo $pr"•••••••••••••"
+echo $cy"5.DARK FB"
+echo $pr"•••••••••••••"
+echo $ij"♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡"
+sleep 1
+echo $ku"•••••••••••••"
+echo $bi"6.DEFACE WEBDAV"
+echo $ku"•••••••••••••"
+echo $me"♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡"
+sleep 1
+echo $ij"•••••••••••••"
+echo $ku"7.INSTALL BAHAN"
+echo $ij"•••••••••••••"
+echo $bi"♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡"
+sleep 1
+echo $pr"•••••••••••••"
+echo $cy"8.INSTALL CMATRIX"
+echo $pr"•••••••••••••"
+echo $bi"♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡"
+sleep 1
+echo $bi"•••••••••••••"
+echo $ku"9.HACK LAMPU"
+echo $bi"•••••••••••••"
+echo $ij"♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡"
+sleep 1
+echo $cy"•••••••••••••"
+echo $pr"10.EXIT TOOLS"
+echo $cy"•••••••••••••"
+echo $bi"♡♡♡♡♡♡♡♡♡♡♡♡♡♡♡"
+sleep 1
+echo $ku"___________________________________________________"
+echo $cy"AUTHOR : TAUFIQ X/꧁☆☬ 🅣🅧•404☬☆•☬"
+echo $cy"TEAM   : NO TEAM"
+echo $cy"THANKS SUDAH MENGGUNAKAN TOOLS INI"
+echo $ku"___________________________________________________"
+read -p"SILAHKAN PILIH TOOLS==>" pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+sleep 1
+git clone https://github.com/BHC-FAKE/BlackHatAttack
+cd BlackHatAttack
+python blackhat.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+sleep 1
+git clone https://github.com/justahackers/perusak
+cd perusak
+python2 perusak.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+sleep 1
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 ipcs.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+sleep 1
+git clone https://github.com/Bl4ckDr460n/ScanIp
+cd ScanIp
+python2 scanip.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+sleep 1
+git clone https://github.com/Yayan-XD/Silent
+cd Silent
+python2 Silent.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+sleep 1
+git clone https://github.com/404rgr/webdav77
+cd webdav77
+bash install.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+sleep 1
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant "B E R H A S I L"|lolcat
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "CMATRIX"|lolcat
+pkg install cmatrix
+cmatrix
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "SUKSES"|lolcat
+git clone https://github.com/LionSec/
+katoolin.git
+ls
+cd katoolin
+ls
+phyton2 katooli.py
+nano katoolin.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "KELUAR"|lolcat
+echo $me"THANKS SUDAH "$ku"MEMAKAI TOOLS INI"
+sleep 1
+echo $cy" SEE YOU NEXT"$pr" TIME AGAIN"
+fi
+
